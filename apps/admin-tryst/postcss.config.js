0d0943940a7ca9b2ffa49208e4d291b02c66c6af
xxKeefer <xxkeefer.code@gmail.com>
2021-08-29 14:14:24 +1000

    feat(tailwindcss): installed
    on both admin and frontend

---
+++ apps/admin-tryst/postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    tailwindcss: { config: './apps/admin-tryst/tailwind.config.js' },
+    autoprefixer: {},
+  },
+};
---
+++ apps/tryst/postcss.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  plugins: {
+    tailwindcss: { config: './apps/tryst/tailwind.config.js' },
+    autoprefixer: {},
+  },
+};
